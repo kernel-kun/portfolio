329d66bf85df014ee9a155485415502116f0a181
kernel-kun <kernel.kun@github.chitkara.edu.in>
2022-08-02 23:02:39 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+/*=============== TYPED JS ===============*/
+let typed = new Typed(".typed", {
+    strings: ["An aspiring Full Stack Developer 👨‍💻", "An aspiring Data Scientist 👨‍🔬", "Python Fanatic 🐱‍🏍", "Internet Junkie 🌐"],
+    typeSpeed: 50,
+    backSpeed: 70,
+    backDelay: 1500,
+    smartBackspace: true,
+    loop: true,
+    cursorChar: '',
+  });
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/* Link active work */ 
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
