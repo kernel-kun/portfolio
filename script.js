bf6762a4c462461073d9d99aa5ae646982bb89d0
kernel-kun <kernel.kun@github.chitkara.edu.in>
2022-08-03 09:11:13 +0530

    Alt Portfolio Project

---
+++ script.js
@@ -0,0 +1,17 @@
+/*=============== typed.js ===============*/
+
+let typed = new Typed(".typed_text", {
+    strings: [
+        " ",
+        "An aspiring Full Stack Developer 👨‍💻",
+        "An aspiring Data Scientist 👨‍🔬",
+        "Python Fanatic 🐱‍🏍",
+        "Internet Junkie 🌐",
+    ],
+    typeSpeed: 50,
+    backSpeed: 70,
+    backDelay: 1500,
+    smartBackspace: true,
+    loop: true,
+    cursorChar: "_",
+});
